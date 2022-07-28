010a0eeb6fea26d1d219bb5a53bdb5ad72fb8bf4
Zakiya <31302217+kmzakiya@users.noreply.github.com>
2022-07-28 23:13:20 +0530

    Update range-counter.js

--- range-counter.js
+++ range-counter.js
@@ -6,9 +6,9 @@ function getRangeCounter(data) {
   let firstElement = data[0];
   let previous = data[0];
   let counter = 1;
-  let output = {};
+  const output = {};
   for (let i = 1; i < data.length; i++) {
-    if ([0,1].includes(data[i] - previous)) {
+    if ([0, 1].includes(data[i] - previous)) {
       previous = data[i];
       counter++;
     } else {
@@ -21,18 +21,18 @@ function getRangeCounter(data) {
   output[`${firstElement}-${previous}`] = counter;
   return output;
 }
-function formatData(key,value){
-    return `${key}, ${value}`;
+function formatData(key, value) {
+  return `${key}, ${value}`;
 }
-function printRangeCounter(){
-    const data = getRangeCounter([2,2,4,3,6,8,7,9,10]);
-    for(const [key,value] of Object.entries(data)){
-        console.log(formatData(key,value));
-    }
+function printRangeCounter() {
+  const data = getRangeCounter([2, 2, 4, 3, 6, 8, 7, 9, 10]);
+  for (const [key, value] of Object.entries(data)) {
+    console.log(formatData(key, value));
+  }
 }
 printRangeCounter();
 module.exports = {
   sort,
   getRangeCounter,
-  formatData
+  formatData,
 };
